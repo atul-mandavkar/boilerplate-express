812e859e389ffa4880e8f45300ba83b54306ad74
atul-mandavkar <76526334+atul-mandavkar@users.noreply.github.com>
2021-08-25 21:07:42 +0530

    Usse of app..sendFile()
    This will Use to transfer another html file tio to the given path in  argument
    
    Here, absolutePath is path where html index file we have to send
    absoluutePath had  __dirname which is  node constant and after this relative path of destination is attached

--- myApp.js
+++ myApp.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var app = express();
-
+var absolutePath = __dirname + "/views/index.html";
 
 console.log("Hello World");
-
+/*
 app.get("/", function(req, res){
   res.send("Hello Express");
+})*/
+
+app.get("/", (req, res)=>{
+  res.sendFile(absolutePath)
 })
 
 
