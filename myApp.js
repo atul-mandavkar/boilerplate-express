fc72188e3f6d738c0c9c598c311bd4a39a731aa1
atul-mandavkar <76526334+atul-mandavkar@users.noreply.github.com>
2021-08-31 21:08:00 +0530

    Root level request logger middleware
    Use app.use (function(req, res, next){}) .
    Here function in app.use acts as middleware , for this use third parameter next(); at the end of function.
    In this example, we log the method name , path and ip for each request in console ,So used next(); mehtod at the end of function so it will work for each request.
    For good result, use app.use() which act as root request logger, above all request
    
    Note: -
    This is analogous method and it is available for all http verbs (like GET, POST, DELETE, PUT, etc)
    Here we used  USE verb only for demonstration.
    If you want this analogous method works for only GET then you can use app.get()
    or only for POST , you can use app.post() and so on.
    But use it before all request starts in code.

--- myApp.js
+++ myApp.js
@@ -4,14 +4,20 @@ var absolutePath = __dirname + "/views/index.html";
 require("dotenv").config();
 console.log("Hello World");
 
-/*
-app.get("/", function(req, res){
+// Root level request logger middleware
+app.use((req, res, next)=>{
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+})
+
+
+app.get("/1", function(req, res){
   res.send("Hello Express");
 })
 
-app.get("/", (req, res)=>{
+app.get("/2", (req, res)=>{
   res.sendFile(absolutePath);
-});*/
+});
 
 /*
 app.use(express.static(__dirname + "/"));
@@ -22,11 +28,11 @@ console.log(__dirname);
 app.use(express.static(__dirname + "/views"));
 // In index.html file the link tag have href attribute is set to /public/style.css so only call root ("/") in next static file implementation and that will directly attach the style.css file to index.html file
 app.use(express.static(__dirname + "/"));
-/*
+
 // Transfer json format
-app.get("/json", (req, res)=>{
+app.get("/json1", (req, res)=>{
   res.json({ message: "Hello json"});
-});*/
+});
 
 // .env for passing enviroment variable
 //console.log(process.env.MESSAGE_STYLE);
