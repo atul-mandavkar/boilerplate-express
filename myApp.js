829e7bea4b931963dd7abd078dd10318c0e6b630
atul-mandavkar <76526334+atul-mandavkar@users.noreply.github.com>
2021-08-31 20:01:24 +0530

    Use the .env file
    .env file is a private file  or secret file.
    To handle this file in express, first install dotenv package,
    In cmd type
    npm i dotenv
    
    Then in that file where we used that data of secret file type only
    require("dotenv").config();
    Then you can use that data by using process.env in that file
    
    The secret file .env is create first. Then in that file type
    MESSAGE_STYLE=uppercase
    
    No need to add space between = sign and semicolon after it's value
    Always write variables of .env file in capital letters with no blank space
    And to write new variable in separate new line because we do not use semicolon

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var absolutePath = __dirname + "/views/index.html";
+require("dotenv").config();
 console.log("Hello World");
 
 /*
@@ -21,13 +22,22 @@ console.log(__dirname);
 app.use(express.static(__dirname + "/views"));
 // In index.html file the link tag have href attribute is set to /public/style.css so only call root ("/") in next static file implementation and that will directly attach the style.css file to index.html file
 app.use(express.static(__dirname + "/"));
-
+/*
 // Transfer json format
 app.get("/json", (req, res)=>{
   res.json({ message: "Hello json"});
-});
+});*/
 
-
+// .env for passing enviroment variable
+//console.log(process.env.MESSAGE_STYLE);
+app.get("/json", (req, res)=>{
+  if(process.env.MESSAGE_STYLE === "uppercase"){
+    res.json({message: "HELLO JSON"});
+  }
+  else{
+    res.json({message: "Hello json"});
+  }
+})
 
 
 
